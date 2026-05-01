6ac48614cc6f14ad1472429ce5ee0d8a867ed539
ruhul1845 <rohul4316@gmail.com>
2026-05-01 18:03:03 +0600

    something changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "https://skillsphere-murex.vercel.app"
+    baseURL: "http://localhost:3000"
 });
 
 export const { signIn, signUp, signOut, updateUser, useSession } = authClient;
