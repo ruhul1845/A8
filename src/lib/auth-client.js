857324cfe019ad2f3a6c173df12437aa926945de
ruhul1845 <rohul4316@gmail.com>
2026-05-01 18:05:43 +0600

    baseURl Fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://skillsphere-murex.vercel.app/"
 });
 
 export const { signIn, signUp, signOut, updateUser, useSession } = authClient;
